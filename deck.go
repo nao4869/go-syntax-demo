cf54b0a10db74290ad2f01df462e721bc6df2397
naohito muraoka <muraokan@oregonstate.edu>
2020-06-06 11:26:02 +0900

    Added: slices, for loop added

--- deck.go
+++ deck.go
@@ -5,10 +5,30 @@ import "fmt"
 // type of 'deck'
 type deck []string
 
+// return value of type deck
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clover"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit + " of " + value)
+		}
+	}
+
+	return cards
+}
+
 // reciever
 // d is instanc of deck
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) {
+	fmt.Println(d)
 }
